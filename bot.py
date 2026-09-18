9b7b1eed2e7e36220be061a35002c4e22c6702a0
alexbaranov201112011-commits <alexbaranov201112011@gmail.com>
2026-09-18 15:28:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,5 @@ def get_signal():
 def send(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": text})
-
-sig = get_signal()
-if sig:
-    send(sig)
+else:
+    send("Тест - бот работает!")
